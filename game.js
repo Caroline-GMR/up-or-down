2b5a9689bcf2ae2d5c9258a03f0b91461ca6564b
Caroline Romero <cgm.romero@gmail.com>
2018-08-29 15:58:52 +0200

    added css

--- game.js
+++ game.js
@@ -5,9 +5,33 @@ function Game(){
  
 Game.prototype.start = function(){
   this.gameMain = buildDom(`
-    <main>
-      <p>this is a new game</p>
-    </main>`
+    <main class="game container">
+    <header>
+      <div class="score">
+        <span class="label">Score: </span>
+        <span class="value">203</span>
+      </div>
+      <div class="timer">
+        <span class="label">Time left: </span>
+        <span class="value">1</span>
+      </div>
+    </header>
+    
+    <div class="deck">
+      <div class="card current-card">2</div>
+      <div class="actions">
+        <button class="up">up</button>
+        <button class="down">down</button>
+      </div>
+      <div class="card next-card">1</div>
+    </div>
+    <footer>
+      <p>
+        <span class="label">Step: </span>
+        <span class="step-no"> 1 </span> / <span class="total-steps"> 51</span>
+      </p>
+    </footer>
+  </main>`
   );
   document.body.appendChild(this.gameMain);
 
